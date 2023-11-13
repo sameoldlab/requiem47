e5e6d1e68a4994f7d3e39a30ca503a797fb9f053
ibro <sameoldlab@proton.me>
2023-11-13 10:04:08 +0100

    object refactor

--- src/boids.ts
+++ src/boids.ts
@@ -1,97 +1,118 @@
 import {vec3, type Boid} from './utils'
 
 export const boidParams = {
-    count: 900,
-    particleSize: 3,
-    speed: 2, //Oddly very high numbers
-    
-    //Consider two level of separate scale; boid to bot  + group to group
-    separate: {
-        threshold:16,
-        strength: 4,
-    }, 
-    align: {
-        radius:   20, //Should be higher than s.threshold
-        strength: 2
-    },
-    cohere: {
-        radius:   12,
-        strength:  0 
-    },
+  count: 1000, //10-13ms / frame
+  particleSize: 3,
+  speed: 2, //Oddly very high numbers seem to work fine
+
+  //Consider two level of separate scale; boid to bot  + group to group
+  separate: {
+    threshold: 16,
+    strength: 4,
+  },
+  align: {
+    radius: 40, //Should be higher than s.threshold
+    strength: 2,
+  },
+  cohere: {
+    radius: 12,
+    strength: 0,
+  },
 }
-const BOUNDS = new vec3(500, 150, 5)
+const BOUNDS = new vec3(500, 150, 500)
 
+// TODO: change to generic name and type
+const boid = (
+  position = new vec3().random().subScalar(0.5).multiplyScalar(200),
+  velocity = new vec3().random().subScalar(0.5).multiplyScalar(99)
+): Boid => ({
+  position,
+  velocity,
+})
 
-export function boid({ position, velocity, acceleration}): Boid {
+function flock(boid, boids: Boid[]) {
+  const { position, velocity } = boid
+  const acceleration = new vec3()
 
-    function bounding() {
-      const vect = new vec3()
-      const {x, y, z } = position
-      const {abs, sign} = Math
-         if (abs(x) > BOUNDS.x) vect.setX(sign(x) * -.5)
-         if (abs(y) > BOUNDS.y) vect.setY(sign(y) * -.5)
-         if (abs(z) > BOUNDS.z) vect.setZ(sign(z) * -.5)      
-        return vect
-    }
+  let totalS = 0,
+    totalA = 0,
+    totalC = 0
+  const separate = new vec3()
+  const align = new vec3()
+  const cohere = new vec3()
 
-    return {
-      position,
-      velocity,
-      acceleration,
-      flock: (boids: Boid[]) => {
-        
-        let totalS = 0, totalA = 0, totalC = 0
-        const separate = new vec3()
-        const align = new vec3()
-        const cohere = new vec3()
+  for (let other of boids) {
+    if (position !== other.position) {
+      const distance = position.distanceTo(other.position)
 
-        for (let other of boids) {
-          if (position !== other.position) {
-            const distance = position.distanceTo(other.position)
-  
-            if (distance < boidParams.separate.threshold) {
-              totalS++            
-              separate.add(new vec3().subVectors(position, other.position)
-                      .divideScalar(distance))
-            }
-            if (distance < boidParams.align.radius) {
-              totalA++
-              align.add(other.velocity)
-            }
-            if (distance < boidParams.cohere.radius) {
-              totalC++
-              cohere.add(other.position)
-            }
-          }
-        }
-        if (totalS) separate.divideScalar(totalS).normalize()
-                            .sub(velocity)
-                            .multiplyScalar(boidParams.separate.strength)
-        // Significantly less interesting without limiting sampling to fov
-        if (totalA) align   .divideScalar(totalA).normalize()
-                            // .sub(velocity)
-                            .multiplyScalar(boidParams.align.strength)
-  
-        if (totalC) cohere  .divideScalar(totalC).normalize()
-                            .multiplyScalar(boidParams.cohere.strength)
-  
-        acceleration
-          .add(separate)
-          .add(align)
-          .add(cohere)
-          .add(bounding())
-          // normalize acceleration. Seems ok without it (both or none) Forms spheres instead of cubes
-          .normalize()
-        //   .multiplyScalar(boidParams.speed)
-      },
-      update: () => {
-        // :facepalm: normalize + multiply is different from clamp
-        velocity.add(acceleration).normalize()
-          .multiplyScalar(boidParams.speed)
-          position.add(velocity)
-        acceleration.multiplyScalar(0)
-
-        return position
-      },
+      if (distance < boidParams.separate.threshold) {
+        totalS++
+        separate.add(
+          new vec3().subVectors(position, other.position).divideScalar(distance)
+        )
+      }
+      if (distance < boidParams.align.radius) {
+        totalA++
+        align.add(other.velocity)
+      }
+      if (distance < boidParams.cohere.radius) {
+        totalC++
+        cohere.add(other.position)
+      }
     }
   }
+  if (totalS)
+    separate
+      .divideScalar(totalS)
+      .normalize()
+      .sub(velocity)
+      .multiplyScalar(boidParams.separate.strength)
+  // Significantly less interesting without limiting sampling to fov
+  if (totalA)
+    align
+      .divideScalar(totalA)
+      .normalize()
+      // .sub(velocity)
+      .multiplyScalar(boidParams.align.strength)
+
+  if (totalC)
+    cohere
+      .divideScalar(totalC)
+      .normalize()
+      .sub(velocity)
+      .multiplyScalar(boidParams.cohere.strength)
+
+  acceleration
+    .add(separate)
+    .add(align)
+    .add(cohere)
+    .add(bounding(position))
+    .divideScalar(3) // add Mass
+    // normalize acceleration. Seems ok without it (both or none) Forms spheres instead of cubes
+    .normalize()
+    .multiplyScalar(boidParams.speed)
+  applyForce(boid, acceleration)
+}
+
+function bounding(position) {
+  const vect = new vec3()
+  const { x, y, z } = position
+  const { abs, sign } = Math
+  if (abs(x) > BOUNDS.x) vect.setX(sign(x) * -15)
+  if (abs(y) > BOUNDS.y) vect.setY(sign(y) * -15)
+  if (abs(z) > BOUNDS.z) vect.setZ(sign(z) * -15)
+  return vect
+}
+
+function applyForce(obj, acceleration) {
+  const { position, velocity } = obj
+  velocity.add(acceleration).normalize().multiplyScalar(boidParams.speed)
+  position.add(velocity)
+  return position
+}
+
+// Flee
+
+// Seek
+
+export { boid, flock }
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,59 @@
 import * as T from 'three'
 const canvas = document.querySelector('canvas.webgl')
 import { Pane } from 'tweakpane'
-import { scene, vec3, type Boid } from './utils'
-import { boid, boidParams } from './boids' 
+import Stats from 'stats.js'
+import { scene, type Boid } from './utils'
+import { boid, boidParams, flock } from './boids'
 
-  const frame = {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  }
-  const mouse = { x: 0, y: 0 }
+const frame = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+const mouse = { x: 0, y: 0 }
 
-  window.addEventListener('resize', () => {
-    // Update sizes
-    frame.width = window.innerWidth
-    frame.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  frame.width = window.innerWidth
+  frame.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = frame.width / frame.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = frame.width / frame.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(frame.width, frame.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-  })
-
-  document.addEventListener('mousemove', (e) => {
-    mouse.x = e.clientX
-    mouse.y = e.clientY
-  })
-
-  const renderer = new T.WebGLRenderer({ canvas })
+  // Update renderer
   renderer.setSize(frame.width, frame.height)
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
-  //////////////////////////////////////////////////////////////
-  //   FLOCKING
-  //////////////////////////////////////////////////////////////
+document.addEventListener('mousemove', (e) => {
+  mouse.x = e.clientX
+  mouse.y = e.clientY
+})
 
-  const flock = [] as Boid[]
-  for (let i = 0; i < boidParams.count; i++) {
-    flock.push(
-      boid({
-        position: new vec3().random().subScalar(0.5).multiplyScalar(200),
-        velocity: new vec3().random().subScalar(0.5).multiplyScalar(99),
-        acceleration: new vec3(),
-      })
-    ) // Do not "optimize"
-  }
+const renderer = new T.WebGLRenderer({ canvas })
+renderer.setSize(frame.width, frame.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+//////////////////////////////////////////////////////////////
+//   FLOCKING
+//////////////////////////////////////////////////////////////
+
+const boids = [] as Boid[]
+for (let i = 0; i < boidParams.count; i++) {
+  boids[i] = boid()
+}
 
   function mesh() {
     const geometry = new T.BufferGeometry()
-    const material = new T.PointsMaterial({ size: boidParams.particleSize })
+    const material = new T.PointsMaterial({
+      size: boidParams.particleSize,
+      fog: true,
+      sizeAttenuation: true,
+    })
 
     // Runs once when added to scene
     const posArray = new Float32Array(boidParams.count * 3)
-    flock.forEach((b, i) => {
+    boids.forEach((b, i) => {
       ;[posArray[i * 3], posArray[i * 3 + 1], posArray[i * 3 + 2]] =
         b.position.toArray()
     })
@@ -65,9 +64,9 @@ import { boid, boidParams } from './boids'
       update: () => {
         const posArray = new Float32Array(boidParams.count * 3)
 
-        flock.forEach((b) => b.flock(flock))
-        flock.forEach((b, i) => {
-          b.update()
+        boids.forEach((b, i) => {
+          flock(b, boids)
+        // });         boids.forEach((b, i) => {
           ;[posArray[i * 3], posArray[i * 3 + 1], posArray[i * 3 + 2]] =
             b.position.toArray()
         })
@@ -98,46 +97,34 @@ import { boid, boidParams } from './boids'
     title: 'Separation',
   })
   sep.addBinding(boidParams.separate, 'threshold', {
-    title: 'dfs',
     min: 0,
     max: 100,
-    step: 1,
   })
   sep.addBinding(boidParams.separate, 'strength', {
-    title: 'dfs',
     min: 0,
     max: 10,
-    step: 1,
   })
   const align = pane.addFolder({
     title: 'Alignment',
   })
   align.addBinding(boidParams.align, 'radius', {
-    title: 'dfs',
     min: 0,
     max: 100,
-    step: 1,
   })
   align.addBinding(boidParams.align, 'strength', {
-    title: 'dfs',
     min: 0,
     max: 10,
-    step: 1,
   })
   const cohere = pane.addFolder({
     title: 'Cohesion',
   })
   cohere.addBinding(boidParams.cohere, 'radius', {
-    title: 'dfs',
     min: 0,
     max: 100,
-    step: 1,
   })
   cohere.addBinding(boidParams.cohere, 'strength', {
-    title: 'dfs',
     min: 0,
     max: 10,
-    step: 1,
   })
 
   // Camera
@@ -155,10 +142,18 @@ import { boid, boidParams } from './boids'
   ///////////////////////////////////////////////////////////////////////////////
 
   const clock = new T.Clock()
-  const animate = () => {
-    particles.update()
+  const stats = Stats()
+//   0: fps, 1: ms, 2: mb, 3+: custom
+  stats.showPanel( 0 );
+  stats.showPanel( 1 );
+//   stats.showPanel( 2 );
+  document.body.appendChild( stats.dom );
+// stats.update()
+  const anime = () => {
+      stats.begin()
+      particles.update()
     // const elapsedTime = clock.getElapsedTime()
-
+    stats.end()
     // camera.rotation.y = 0.5 * elapsedTime
 
     // Render
@@ -166,4 +161,23 @@ import { boid, boidParams } from './boids'
     renderer.render(scene, camera)
   }
 
-  animate()
+//   anime()
+
+
+const frameLengthMS = 1000/70;//60 fps
+let previousTime = 0;
+// The previous answers seem to ignore the intended design of requestAnimationFrame, and make some extraneous calls as a result. requestAnimationFrame takes a callback, which in turn takes a high precision timestamp as its argument. So you know the current time and you don't need to call Date.now(), or any other variant, since you already have the time. All that’s needed is basic arithmetic:
+function animate(timestamp=0){
+    if(timestamp - previousTime > frameLengthMS){
+    //   console.log(previousTime)
+   stats.begin()
+   particles.update()
+   // const elapsedTime = clock.getElapsedTime()
+   stats.end()  
+    previousTime = timestamp;
+  }
+  window.requestAnimationFrame(animate)
+  renderer.render(scene, camera)
+
+}
+animate()
