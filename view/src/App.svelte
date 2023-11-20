c067e2500b24910745098bd75a75d6e65145e26f
ibro <sameoldlab@proton.me>
2023-11-20 23:31:35 +0100

    Minimum Viable scheme

--- view/src/App.svelte
+++ view/src/App.svelte
@@ -1,17 +1,42 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-
-	let canvas: HTMLCanvasElement
-	let ctx: CanvasRenderingContext2D
-	onMount(()=> {
-        if (!canvas) return Error('canvas not found')
-		ctx = canvas.getContext('2d')
-	})
-    	
+	import {Canvas, Layer, type Render} from 'svelte-canvas'
+	let canvas: HTMLCanvasElement 
+	let render: Render
+	const PPI = 20
+ 	const map = (x, b, d) => x/b * d;
+	   	
+	$: render =({context: ctx, width, height}) => {
+		console.log(height, width)
+		
+		ctx.font = `${width / 10}px sans-serif`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = 'tomato';
+    ctx.fillText('hello world', width / 2, height / 2);
+	
+		for(let x =0; x<PPI; x++) {
+			for(let y =0; y<PPI; y++) {
+							
+				ctx.fillStyle = `oklch(
+					37,
+					.5,
+					${(Math.random()*220)+120} 
+				)`;
+				//Convert hue to an array of preset values
+				ctx.fillRect(
+					map(x, PPI, width),
+					map(y, PPI, height), 
+					width/PPI, 
+					height/PPI,
+				)
 			
+		}}
+	}		
 </script>
 	<div class="view">
-		<canvas bind:this={canvas}/>
+		<Canvas bind:this={canvas}>
+			<Layer {render}/>
+		</Canvas>
 		<div class="view-controls">.</div>
 	</div>
 
